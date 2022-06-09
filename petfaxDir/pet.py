c7d0a40fa0beb93a6ba12c76ca8be54793d38090
Lenore Kelly <lenorekelly2@gmail.com>
2022-06-09 19:18:05 -0400

    getting there :)

--- petfaxDir/pet.py
+++ petfaxDir/pet.py
@@ -8,4 +8,4 @@ bp = Blueprint('pet', __name__, url_prefix="/pets")
 
 @bp.route('/')
 def index():
-    return render_template('index.html', pets=pets)
+    return render_template('pets/index.html', pets=pets)
